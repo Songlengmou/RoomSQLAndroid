08529160ee392556955cc14dd2fa3af3d4388fec
Administrator <1020492220@qq.com>
2021-01-27 11:09:25 +0800

    删除某张表

--- app/src/main/java/com/anningtex/roomsqlandroid/db/PhoneDatabase.java
+++ app/src/main/java/com/anningtex/roomsqlandroid/db/PhoneDatabase.java
@@ -20,7 +20,7 @@ import com.anningtex.roomsqlandroid.dao.PhoneDao;
  * @author Administrator
  */
 @Database(entities = {PhoneBean.class, NewReport.class, TwoReportBean.class, OrderSpecBean.class},
-        version = 9, exportSchema = false)
+        version = 10, exportSchema = false)
 @TypeConverters({ConversionFactory.class})
 public abstract class PhoneDatabase extends RoomDatabase {
     private static final String DATABASE_NAME = "PHONE.db";
@@ -31,9 +31,9 @@ public abstract class PhoneDatabase extends RoomDatabase {
 
     private static PhoneDatabase buildDatabase(Context context) {
         return Room.databaseBuilder(context.getApplicationContext(), PhoneDatabase.class, DATABASE_NAME)
-                .fallbackToDestructiveMigration()
+//                .fallbackToDestructiveMigration()
                 .allowMainThreadQueries()
-                .addMigrations(MIGRATION_1_2, MIGRATION_2_3, MIGRATION_3_4, MIGRATION_4_5, MIGRATION_5_6, MIGRATION_6_7)
+                .addMigrations(MIGRATION_1_2, MIGRATION_2_3, MIGRATION_3_4, MIGRATION_4_5, MIGRATION_5_6, MIGRATION_6_7,MIGRATION_9_10)
                 .build();
     }
 
@@ -126,4 +126,12 @@ public abstract class PhoneDatabase extends RoomDatabase {
      * 不需要写上方的这些Migration，直接创建一个新表,升级数据库版本进行调用
      */
     public abstract OrderSpecDao getOrderSpecDao();
+
+    static final Migration MIGRATION_9_10 = new Migration(9, 10) {
+        @Override
+        public void migrate(SupportSQLiteDatabase database) {
+            //删除某张表
+            database.execSQL("DROP TABLE TwoReportTemp ");
+        }
+    };
 }
